62c855a18d82bbe87292c64c28d784330b56257b
Louis V <112769491+LouisVct@users.noreply.github.com>
2026-01-30 22:52:15 -0500

    Added the first program to make the characters appear on the screen + update of the required libraries.

---
+++ main.py
@@ -0,0 +1,47 @@
+import pygame
+import sys
+
+
+pygame.init()
+
+police = "verdana" 
+
+W, H = 1080, 720
+backgroundColor = "#FAF9F6"
+fontColor = "black"
+screen = pygame.display.set_mode((W, H))
+pygame.display.set_caption("Doigts rapide - test saisie")
+
+
+font = pygame.font.SysFont(police, 35)
+
+input_text = ""
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    screen.fill(backgroundColor)
+    clock.tick(60) # écran 60 fps max, donc pas besoin plus
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        if event.type == pygame.TEXTINPUT:
+            input_text += event.text
+            
+        
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE:
+                input_text = input_text[:-1]
+
+    
+    
+    txt_surf = font.render(input_text, True, fontColor)
+    screen.blit(txt_surf, (20, 30))
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
