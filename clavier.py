21e484173aa19ffdca6613b7af7f2595af0e71b1
Louis V <112769491+LouisVct@users.noreply.github.com>
2026-02-24 16:25:19 -0500

    ✅ à partir de ce fichier JSON, je suis venu créer clavier.py qui contient une classe Touche ainsi que le modèle qui contiendra l’intégralité des touches du clavier ainsi que ses informations.

---
+++ clavier.py
@@ -0,0 +1,71 @@
+import json
+
+
+class Touche: 
+    def __init__(self, caracteres, hauteur, largeur, frame_color =(0,0,0) , background_color = None, text_color = (0,0,0)):
+        self.caracteres = caracteres
+        self.hauteur = hauteur
+        self.largeur = largeur
+        self.frame_color = frame_color
+        self.background_color = background_color
+        self.text_color = text_color
+        self.rect = None
+
+
+
+class ModeleClavier:
+    def __init__(self, chemin_json):
+        self.lignes_touches = []       # Liste de listes (pour l'affichage ligne par ligne)
+        self.dictionnaire_touches = {} # Dictionnaire (pour la recherche ultra-rapide)
+        
+        self.charger_depuis_json(chemin_json)
+
+    def charger_depuis_json(self, chemin):
+       
+        with open(chemin, 'r', encoding='utf-8') as f: # Ouverture du fichier JSON en lecture
+            donnees = json.load(f)
+
+        # Récupération des données globales
+        self.w_reel = donnees["largeur_réel_clavier"]
+        self.h_reel = donnees["hauteur_réel_clavier"]
+        w_defaut = donnees["largeur_par_default"]
+        h_defaut = donnees["hauteur_par_default"]
+
+        # On parcourt les lignes du JSON
+        for ligne_json in donnees["touches"]:
+            ligne_objets = []
+            
+            # On parcourt chaque touche dans la ligne
+            for t_json in ligne_json:
+                
+                # Récupération des valeurs du JSON et utilisation des valeurs par défaut si nécessaire
+                caracs = t_json["caracteres"]
+                w = t_json.get("largeur", w_defaut)
+                h = t_json.get("hauteur", h_defaut)
+                
+                #Récupération des couleurs, si il n'y en à pas on met des valeurs par défaut
+                frame = t_json.get("frame_color", (0, 0, 0))
+                bg = t_json.get("background_color", None)
+                txt = t_json.get("text_color", (0, 0, 0))
+                
+                # Instanciation d'une touche
+                nouvelle_touche = Touche(
+                    caracteres=caracs,
+                    hauteur=h,          
+                    largeur=w,          
+                    frame_color=frame,
+                    background_color=bg,
+                    text_color=txt
+                )
+                
+                # Ajout de la touche à la liste (pour pygame)
+                ligne_objets.append(nouvelle_touche)
+                
+                # ajout des tout les caractères de notre touche dans le dictionnaire
+                for c in caracs:
+                    cle = c.lower()
+                    self.dictionnaire_touches[cle] = nouvelle_touche
+
+            # On ajoute la ligne terminée à notre liste globale
+            self.lignes_touches.append(ligne_objets)
+
